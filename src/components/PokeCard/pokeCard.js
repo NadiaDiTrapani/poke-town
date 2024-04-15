f9f2ec1639eb447e565edb15e589caaa0477991a
Nadia Di Trapani <nditrapani@gmail.com>
2024-04-15 17:25:23 -0400

    pulled info to fill out pokecard in pokedex

--- src/components/PokeCard/pokeCard.js
+++ src/components/PokeCard/pokeCard.js
@@ -1,18 +1,35 @@
-import './pokeCard.scss'
+import './pokeCard.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-import pic from '../../assets/images/testEevee.png'
+function PokeCard({ pokemon }) {
+    const [details, setDetails] = useState(null);
 
-function PokeCard(){
-    return(
-        <div> 
-            <div className='pokecard'>
-                <p className='pokecard__id'>123</p>
-                <img src={pic} alt='pokemon' className='pokecard__image'/>
-                <p className='pokecard__name'>pokemon Name</p>
-                <p className='pokecard__type'>pokemon type</p>
-            </div>
+    useEffect(() => {
+        const getDetails = async () => {
+            try {
+                const response = await axios.get(pokemon.url);
+                setDetails(response.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        getDetails();
+    }, [pokemon]);
+
+    if (!details) {
+        return <div>Getting Pokémon... Please hold tight!</div>;
+    }
+
+    return (
+        <div className='pokecard'>
+            <p className='pokecard__id'>{details.id}</p>
+            <img src={details.sprites.front_default} alt='pokemon' className='pokecard__image'/>
+            <p className='pokecard__name'>{details.name}</p>
+            <p className='pokecard__type'>{details.types[0].type.name}</p>
         </div>
-    )
+    );
 }
 
 export default PokeCard;
--- src/pages/pokedex/pokedex.js
+++ src/pages/pokedex/pokedex.js
@@ -1,18 +1,43 @@
 import PokeCard from '../../components/PokeCard/pokeCard';
-import './pokedex.scss'
-
-// import axios from 'axios';
+import './pokedex.scss';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 function Pokedex(){
+    const { id } = useParams();
+    const [details, setDetails] = useState([]);
+
+    useEffect(()=>{
+        const getDetails = async() =>{
+            try{
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+                setDetails(response.data.results);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getDetails();
+    },[id]);
+
+    if (!details){
+        return <div>Getting Pokémon... Please hold tight!</div>;
+    }
+
     return(
         <div className='pokedex'>
             <div className='pokedex__title-cont'>
-                <h1 className='pokedex__title'>Kanto region pokédex</h1>
+                <h1 className='pokedex__title'>Kanto region Pokédex</h1>
             </div>
 
-            <PokeCard/>
+            {details.map((pokemon, index) => (
+                <div key={index}>
+                    <PokeCard pokemon={pokemon}/>
+                </div>
+            ))}
+            
         </div>
-    )
+    );
 }
 
 export default Pokedex;
